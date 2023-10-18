c9f12acce82c7d1f9fe0f82be9df34fc9d4da675
Jardel_MD <jardeldylewski@hotmail.com>
2023-10-18 12:37:40 -0300

    fix: Boas práticas

--- src/components/ProtectedRoutes/index.jsx
+++ src/components/ProtectedRoutes/index.jsx
@@ -1,11 +1,9 @@
-import { useContext } from "react";
-import { Outlet, Navigate } from "react-router-dom";
-import { UserContext } from "../../providers/UserContext";
+import { useContext } from "react"
+import { Outlet, Navigate } from "react-router-dom"
+import { UserContext } from "../../providers/UserContext"
 
-//Proteção de rotas
 export const ProtectedRoutes = () => {
-    const { user } = useContext(UserContext);
- 
-    //Se o usuário existir, renderiza a rota filho. Caso contrário, redireciona para a página inicial
+    const { user } = useContext(UserContext)
+
     return user ? <Outlet /> : <Navigate to="/" />
 }
--- src/providers/UserContext.jsx
+++ src/providers/UserContext.jsx
@@ -1,84 +1,75 @@
 import { createContext, useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom";
-import { api } from "../services/api";
+import { useNavigate } from "react-router-dom"
+import { api } from "../services/api"
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-export const UserContext = createContext({});
+export const UserContext = createContext({})
 
 export const UserProvider = ({ children }) => {
-    const navigate = useNavigate()
-    // Estado de usuário:
-    const [user, setUser] = useState(null)
+  const navigate = useNavigate()
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(false)
 
-      //Efeito de montagem para deixar o usuário persistente na página (Realizar uma requisição para trazer o usuário de volta)
-   useEffect(() => {
+  useEffect(() => {
     const loadUser = async () => {
-       const token = localStorage.getItem("@TOKEN");
-  
-
-       //Tentativa de requisição, caso o usuário exista e tenha um identificador
-       if (token) {
-          try {
-             const { data } = await api.get("/profile", {
-                headers: {
-                   Authorization: `Bearer ${token}`,
-                },
-             });
-             setUser(data);//Realimentar o estado
-             navigate("/dashboard");//Navegar para a dashboard
-          } catch (error) {//Se a requisição não der certo, limpa as chaves do localStorage para evitar possíveis conflitos
-             toast.error(error);
-             localStorage.removeItem("@TOKEN");
-          }
-       }
-    };
-    loadUser();
- }, []); 
-    
-    //Função de Cadastro:
-    const [loading, setLoading] = useState(false)
-    const userRegister = async (formData) => {
+      const token = localStorage.getItem("@TOKEN")
+      if (token) {
         try {
-            setLoading(true)
-            const { data } = await api.post('/users', formData)
-            navigate("/")
-            toast.success("Conta criada com sucesso!")
+          const { data } = await api.get("/profile", {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          })
+          setUser(data)
+          navigate("/dashboard")
         } catch (error) {
-            toast.error("Ops! Algo deu errado")
-
-        } finally {
-            setLoading(false)
+          console.log(error)
+          localStorage.removeItem("@TOKEN")
         }
-    }
-
-    //Função de Login:
-    const userLogin = async (formData) => {
-      try {
-        setLoading(true)
-        const { data } = await api.post("/sessions", formData)
-        setUser(data.user)
-        localStorage.setItem("@TOKEN", data.token)
-        navigate("/dashboard")
-        toast.success(`Bem vindo ${data.user.name}!`)
-      } catch (error) {
-        toast.error("Email ou senha incorretos. Tente novamente!")
-      } finally {
-        setLoading(false)
       }
     }
+    loadUser()
+  }, [])
 
-    //Função de Logout:
-    const userLogout = () => {
-        setUser(null);
-        localStorage.removeItem("@TOKEN");
-        navigate("/")
+  const userRegister = async (formData) => {
+    try {
+      setLoading(true)
+      const { data } = await api.post('/users', formData)
+      navigate("/")
+      toast.success("Conta criada com sucesso!")
+    } catch (error) {
+      toast.error("Ops! Algo deu errado")
+
+    } finally {
+      setLoading(false)
     }
+  }
 
-   
-    return (
-        <UserContext.Provider value={{user, setUser, userRegister, userLogin, userLogout }}>
-            {children}
-        </UserContext.Provider>
-    )
+  const userLogin = async (formData) => {
+    try {
+      setLoading(true)
+      const { data } = await api.post("/sessions", formData)
+      setUser(data.user)
+      localStorage.setItem("@TOKEN", data.token)
+      navigate("/dashboard")
+      toast.success(`Bem vindo ${data.user.name}!`)
+    } catch (error) {
+      toast.error("Email ou senha incorretos. Tente novamente!")
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const userLogout = () => {
+    setUser(null)
+    localStorage.removeItem("@TOKEN")
+    navigate("/")
+  }
+
+  return (
+    <UserContext.Provider value={{ user, setUser, userRegister, userLogin, userLogout }}>
+      {children}
+    </UserContext.Provider>
+  )
 }
